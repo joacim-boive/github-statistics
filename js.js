423d9ac5986af8abcce96950e582cc6d229019fc
joacim.boive@gmail.com <gsxr750>
2017-02-20 12:32:59 +0100

    Converted to ES6 Got all reviews in a structure

--- js.js
+++ js.js
@@ -1,27 +1,57 @@
+'use strict';
+
+let users = {};
+let lookups = [];
+
 const headers = new Headers({
-    'Authorization':'Basic bGVuc3dheTpzaDB3bTNkNGMwZGV6'
-});
-const request = new Request('https://api.github.com/repos/lensway/lensway-all/pulls', {
-    method: 'GET',
-    redirect: 'follow',
-    headers: headers
+    'Authorization': 'Basic bGVuc3dheTpzaDB3bTNkNGMwZGV6'
 });
 
-fetch(request)
-    .then(function(response) {return response.json()})
-    .then(function(json){
-        let prs = [];
-
+fetch(
+    'https://api.github.com/repos/lensway/lensway-all/pulls?state=all&page=1&per_page=100', {
+        method: 'GET',
+        redirect: 'follow',
+        headers: headers
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(json => {
         headers.append('Accept', 'application/vnd.github.black-cat-preview+json');
 
-        for(let pr of json){
-            fetch(`https://api.github.com/repos/lensway/lensway-all/pulls/${pr.number}/reviews`, {
+        for (let pr of json) {
+            lookups.push(fetch(`https://api.github.com/repos/lensway/lensway-all/pulls/${pr.number}/reviews`, {
                 method: 'GET',
                 redirect: 'follow',
                 headers: headers
-            }).then(function(response) {return response.json()})
-                .then(function(json){
-                    debugger;
-                })
+            }).then(response => {
+                return response.json()
+            }).then(reviews => {
+                for (let review of reviews) {
+                    let thisDate = new Date(new Date(review.submitted_at).setHours(0, 0, 0, 0));
+
+                    if (users[review.user.login]) {
+                        if (users[review.user.login][thisDate]) {
+                            users[review.user.login][thisDate].count++;
+                        } else {
+                            users[review.user.login][thisDate] = {};
+                            users[review.user.login][thisDate].count = 1;
+                        }
+                    } else {
+                        users[review.user.login] = {};
+                        users[review.user.login][thisDate] = {};
+                        users[review.user.login][thisDate].count = 1;
+                    }
+                }
+
+            }));
         }
+
+        Promise.all(lookups).then(() => {
+            debugger;
+            console.table(users);
+        }, reason => {
+            debugger;
+            console.log(reason);
+        })
     });
